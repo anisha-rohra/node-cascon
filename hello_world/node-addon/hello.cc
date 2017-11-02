5afde4f87f51ec106f99a0fb4cb59044dfacf204
John Barboza <jbarboza@ca.ibm.com>
2017-11-02 11:33:34 -0400

    add comments to source files

--- hello_world/node-addon/hello.cc
+++ hello_world/node-addon/hello.cc
@@ -1,18 +1,37 @@
+/* Use the header files from node and v8 */
 #include <>
 #include <>
 
 
+/* Use the v8 namespace */
 
+/* Set this function to receive a FuncionCallbackInfo value */
 void Method() {
 
+  /* Get the current instance of the v8 isolate */
 
+  /* Create a handle scope */
 
+  /* Create a JavaScript string called 'world' in the isolate */
+
+  /* Set the JavaScript string as the the return value */
 }
 
+/* Set this function to receive an object handle called 'exports' */
 void Init() {
 
+  /* Get the current instance of the v8 isolate */
 
+  /* Create a JavaScript string called 'hello' in the isolate */
 
+  /* Create a JavaScript function in the isolate that is
+     bound to  the 'Method' function above in the isolate
+  */
+
+  /* Create a property in exports with the 'hello' string and then assign it
+     with the 'Method' JavaScript function
+  */ 
 }
 
+/* Set this module to invoke the 'Init' function on creation*/
 NODE_MODULE()
---
+++ hello_world/node-addon/hello.js
@@ -0,0 +1,5 @@
+/* Get a reference to the native modiule */
+
+/* Invoke the hello function and store return value in a variable */
+
+/* Print the received value to screen */
