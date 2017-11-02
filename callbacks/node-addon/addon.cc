d8fb2f38027b1e763ee7c13f59cb4b62263b0b02
Anisha Rohra <Anisha.Rohra@ibm.com>
2017-11-02 01:17:08 -0400

    updated callback skeleton code to include more base code

--- callbacks/node-addon/addon.cc
+++ callbacks/node-addon/addon.cc
@@ -1,9 +1,11 @@
-#include <>
-#include <>
+#include <node.h>
+#include <v8.h>
 
+using namespace v8;
 
-void RunCallback() {
-
+void RunCallback(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
 
 
 
@@ -12,7 +14,9 @@ void RunCallback() {
 }
 
 void Init() {
-
+  Isolate* isolate = Isolate::GetCurrent();
+  exports->Set(String::NewFromUtf8(isolate, "callback"),
+               FunctionTemplate::New(isolate, RunCallback)->GetFunction());
 }
 
-NODE_MODULE()
+NODE_MODULE(addon, Init)
