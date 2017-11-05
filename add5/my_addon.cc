5751e91a08de287c24c037e4d99db9a544f43a9d
Anisha Rohra <Anisha.Rohra@ibm.com>
2017-11-05 18:08:40 -0500

    changing the structure of the directories, naming of the examples, added add5 example

---
+++ add5/my_addon.cc
@@ -0,0 +1,26 @@
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+void Add5Method() {
+
+
+
+
+
+
+
+
+}
+
+void Add5Init() {
+
+
+
+
+
+
+}
+
+NODE_MODULE()
---
+++ hello_world/hello.cc
@@ -0,0 +1,18 @@
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+void HelloWorldMethod() {
+
+
+
+}
+
+void HelloWorldInit() {
+
+
+
+}
+
+NODE_MODULE()
